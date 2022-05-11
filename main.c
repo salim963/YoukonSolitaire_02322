02e89b3bfc0c7992128604181935ed6bad05bc47
salim963 <70900247+salim963@users.noreply.github.com>
2022-05-11 11:12:10 +0200

    CARDS MOVING

--- main.c
+++ main.c
@@ -1,58 +1,15 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include "cards.h"
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
 
+#include "game.h"
 
-_Noreturn void go();
+/************************************************<Shows All The Cards>***********************************************/
+/**
+ * @authors Omar,
+ * @param LoadTheGame to load the game.
+ * */
 
 int main() {
-    printf("Welcome to Yukon Solitaire game\nGroup16 \n\n");
 
-    go();
+    LoadTheGame();
 
     return 0;
-}
-
-
-void go() {
-    char playCommand[2];
-    int newGame = 0;
-    while (true){
-            printf("INPUT >");
-            scanf("%s",playCommand);
-
-        Node * cardsDeckHead;
-// if we start a new game than we should create a new deck
-        if (newGame == 0) {
-            cardsDeckHead ;
-        }else{
-
-        }
-
-//
-//        if(!strcmp(playCommand,"SW")){
-//            printf("Last message: \n",playCommand);
-//        }
-
-    }
-
-}
-
-struct node* CreateDeck(){
-    Node* theHead = NodeCreate();
-    Card tempCard;
-    Node * temp;
-    tempCard.visibility = 1; // 1 means visibility
-    // we need two loop to create a deck for cards
-    for(int index=1 ; index<5; index++){
-        if(index==1){ tempCard.suit = 'H';}
-        if(index==2){ tempCard.suit = 'D';}
-        if(index==3){ tempCard.suit = 'C';}
-        if(index==4){ tempCard.suit = 'S';}
-
-
-    }
 }
